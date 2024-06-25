f9dde0485734c76d4a8b7ac2517c7abf34e47b1b
diego <diego.berardi02@gmail.com>
2024-06-25 22:03:10 +0200

    immagini professor/s

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-insert into student(id,name,surname,age,description) values(nextval('student_seq'),'Gino','Rossi',21,'studente in cerca di ripetizioni')
-insert into student(id,name,surname,age,description) values(nextval('student_seq'),'Dino','Verdi',18,'studente in cerca di ripetizioni')
+-- insert into student(id,name,surname,age,description) values(nextval('student_seq'),'Gino','Rossi',21,'studente in cerca di ripetizioni')
+-- insert into student(id,name,surname,age,description) values(nextval('student_seq'),'Dino','Verdi',18,'studente in cerca di ripetizioni')
 
 
-insert into professor(id,name,surname,description,age) values(nextval('professor_seq'),'Aldo','Bianchi','laureato in ingegneria',25)
-insert into professor(id,name,surname,description,age) values(nextval('professor_seq'),'Gigi','Nero','laureato in scienze delle zoologie marine',85)
+-- insert into professor(id,name,surname,description,age) values(nextval('professor_seq'),'Aldo','Bianchi','laureato in ingegneria',25)
+-- insert into professor(id,name,surname,description,age) values(nextval('professor_seq'),'Gigi','Nero','laureato in scienze delle zoologie marine',85)
