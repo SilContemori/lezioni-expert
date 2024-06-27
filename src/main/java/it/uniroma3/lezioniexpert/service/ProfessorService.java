21724202ad7c6e0aca585c484164ee083da54eb2
diego <diego.berardi02@gmail.com>
2024-06-27 13:57:51 +0200

    refactor

--- src/main/java/it/uniroma3/lezioniexpert/service/ProfessorService.java
+++ src/main/java/it/uniroma3/lezioniexpert/service/ProfessorService.java
@@ -7,14 +7,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import it.uniroma3.lezioniexpert.model.Professor;
-import it.uniroma3.lezioniexpert.model.User;
 import it.uniroma3.lezioniexpert.repository.ProfessorRepository;
-import it.uniroma3.lezioniexpert.repository.UserRepository;
 
 
 @Service
