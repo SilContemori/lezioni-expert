b1950fff61bc955d8590d43d7d055624584a5f14
SilContemori <silviacontemori31@gmail.com>
2024-06-27 16:59:28 +0200

    commit non funzionante

--- src/main/java/it/uniroma3/lezioniexpert/controller/ProfessorController.java
+++ src/main/java/it/uniroma3/lezioniexpert/controller/ProfessorController.java
@@ -5,12 +5,21 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import it.uniroma3.lezioniexpert.model.Credentials;
+import it.uniroma3.lezioniexpert.model.Images;
+import it.uniroma3.lezioniexpert.model.Professor;
+import it.uniroma3.lezioniexpert.repository.ImagesRepository;
 import it.uniroma3.lezioniexpert.repository.ProfessorRepository;
 import it.uniroma3.lezioniexpert.service.CredentialsService;
+import jakarta.validation.Valid;
 import it.uniroma3.lezioniexpert.repository.UserRepository;
 
 @Controller
@@ -18,6 +27,7 @@ public class ProfessorController {
 	@Autowired ProfessorRepository professorRepository;
 	@Autowired UserRepository userRepository;
 	@Autowired CredentialsService credentialsService;
+	@Autowired ImagesRepository imageRepository;
 	
 	
 	/*GET DELLA PAGINA CON LA LISTA DI TUTTI GLI STUDENTI*/
@@ -63,6 +73,58 @@ public class ProfessorController {
 			return "adminProfilePage.html";
 		}
 	}
+	
+	/*GET E POST PER AGGIUNGERE UN PROFESSORE*/
+//	@GetMapping(value="/admin/formNewProfessor")
+//	public String formNewProfessor(Model model) {
+//		model.addAttribute("professor", new Professor());
+//		return "admin/formNewProfessor.html";
+//	}
+//
+//	@PostMapping(value={"/admin/professor"},consumes = "multipart/form-data")
+//	public String NewProfessor(@Valid @ModelAttribute Professor professor,@RequestPart("file") MultipartFile file, BindingResult bindingResult, Model model) {
+//		//		this.movieValidator.validate(movie, bindingResult);
+//		System.out.println("------------------------------------------------");
+//		if (!bindingResult.hasErrors()) {
+//			try {
+//				Images i=new Images();
+//				i.setImageData(file.getBytes());
+//				professor.setProfileImage(i);
+//				this.imageRepository.save(i);
+//			} catch (Exception e) {
+//				System.out.println("erroreeee");
+//			}
+//			this.professorRepository.save(professor);
+//			return "redirect:/professor/"+professor.getId();
+//		} else {
+//			return "admin/formNewProfessor.html"; 
+//		}
+//	}	
+	@GetMapping(value="/admin/formNewProfessor")
+    public String formNewProfessor(Model model) {
+        model.addAttribute("professor", new Professor());
+        return "admin/formNewProfessor.html";
+    }
+
+	@PostMapping(value = { "/admin/aggiungiProfessor" }, consumes = "multipart/form-data")
+    public String newProfessor(@Valid @ModelAttribute Professor professor, @RequestPart("file") MultipartFile file, BindingResult bindingResult, Model model) {
+        System.out.println("------------------------------------------------");
+        if (!bindingResult.hasErrors()) {
+            try {
+                Images i = new Images();
+                i.setImageData(file.getBytes());
+                professor.setProfileImage(i);
+                this.imageRepository.save(i);
+            } catch (Exception e) {
+                System.out.println("erroreeee");
+            }
+            this.professorRepository.save(professor);
+            return "redirect:/professor/" + professor.getId();
+        } else {
+            return "admin/formNewProfessor.html";
+        }
+    }
+	/*GET PER SETTARE UN PROFESSORE A UN'ANNUNCIO*/
 	
 }
 
