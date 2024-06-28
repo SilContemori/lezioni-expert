14d51be0b2da16434c9d297cdeb86722c1c1f7cc
SilContemori <silviacontemori31@gmail.com>
2024-06-28 16:28:05 +0200

    aggiungi e rimuovi professore
    + aggiustamenti link

--- src/main/java/it/uniroma3/lezioniexpert/controller/ProfessorController.java
+++ src/main/java/it/uniroma3/lezioniexpert/controller/ProfessorController.java
@@ -8,14 +8,24 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 
 import it.uniroma3.lezioniexpert.model.Announcement;
 import it.uniroma3.lezioniexpert.model.Credentials;
+import it.uniroma3.lezioniexpert.model.Images;
 import it.uniroma3.lezioniexpert.model.Professor;
+import it.uniroma3.lezioniexpert.repository.AnnouncementRepository;
+import it.uniroma3.lezioniexpert.repository.CredentialsRepository;
+import it.uniroma3.lezioniexpert.repository.ImagesRepository;
 import it.uniroma3.lezioniexpert.repository.ProfessorRepository;
 import it.uniroma3.lezioniexpert.service.CredentialsService;
+import jakarta.validation.Valid;
 import it.uniroma3.lezioniexpert.repository.UserRepository;
 
 @Controller
@@ -23,6 +33,9 @@ public class ProfessorController {
 	@Autowired ProfessorRepository professorRepository;
 	@Autowired UserRepository userRepository;
 	@Autowired CredentialsService credentialsService;
+	@Autowired ImagesRepository imageRepository;
+	@Autowired CredentialsRepository credentialsRepository;
+	@Autowired AnnouncementRepository announcementRepository;
 	
 	
 	/*GET DELLA PAGINA CON LA LISTA DI TUTTI GLI STUDENTI*/
@@ -80,6 +93,80 @@ public class ProfessorController {
 			model.addAttribute("user", this.userRepository.findById(credentials.getUser().getId()).get());
 			return "adminProfilePage.html";
 		}
+	}
+	
+	/*GET E POST PER AGGIUNGERE UN NUOVO PROFESSORE*/
+	@GetMapping(value="/formNewProfessor")
+	public String formNewProfessor(Model model) {
+		model.addAttribute("professor", new Professor());
+		return "formNewProfessor.html";
+	}
+
+	@PostMapping(value={"/professor"},consumes = "multipart/form-data")
+	public String newProfessor(@Valid @ModelAttribute Professor professor,@RequestPart("file") MultipartFile file, BindingResult bindingResult, Model model) {
+		//		this.movieValidator.validate(movie, bindingResult);
+		if (!bindingResult.hasErrors()) {
+			try {
+				Images i=new Images();
+				i.setImageData(file.getBytes());
+				professor.setProfileImage(i);
+				this.imageRepository.save(i);
+			} catch (Exception e) {
+				System.out.println("erroreeee");
+			}
+			this.professorRepository.save(professor);
+			return "redirect:/professor/"+professor.getId();
+		} else {
+			return "formNewProfessor.html"; 
+		}
+	}
+	
+	/*GET PER RIMUOVERE UN PROFESSORE E TUTTI I SUOI ANNUNCI*/
+	@GetMapping("/removeProfessor/{idProfessor}")
+	public String removeProfessor(@PathVariable("idProfessor") Long idProfessor, Model model) {
+		UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    	Credentials credentials = credentialsService.getCredentials(userDetails.getUsername());
+		Professor professor= this.professorRepository.findById(idProfessor).get();
+		List<Announcement> announcements = professor.getAnnouncements();
+		for(Announcement a: announcements) {
+			a.setProfessor(null);
+		}
+	    this.professorRepository.save(professor);
+		Iterable<Credentials> allCredentials = this.credentialsRepository.findAll();
+		for(Credentials i: allCredentials) {
+			if(i.getProfessor() != null) {
+				if(i.getProfessor().getId() == idProfessor) {
+					if(!i.getRole().equals(Credentials.ADMIN_ROLE)) {
+		                i.setProfessor(null);
+		                this.credentialsRepository.delete(i);
+		            }
+				}
+			}
+		}
+		this.professorRepository.delete(professor);
+		model.addAttribute("user", this.userRepository.findById(credentials.getUser().getId()).get());
+		return "adminProfilePage.html";
+	}
+	
+	/*GET PER SETTARE UN PROFESSORE A UN ANNUNCIO ESISTENTE*/
+	@GetMapping(value="/setProfessor/{idAnnouncement}")
+	public String setProfessor(@PathVariable Long idAnnouncement,Model model) {
+		Announcement a=announcementRepository.findById(idAnnouncement).orElse(null);
+		model.addAttribute("professors",this.professorRepository.findAll());
+		model.addAttribute("announcement",a);
+		return "setProfessor.html";
+	}
+	
+	@GetMapping(value="/setProfessor/{idAnnouncement}/{idProfessor}")
+	public String setProfessorAnnouncement(@PathVariable Long idAnnouncement,@PathVariable Long idProfessor,Model model) {
+		Announcement a=announcementRepository.findById(idAnnouncement).orElse(null);
+		Professor p=professorRepository.findById(idProfessor).orElse(null);
+		a.setProfessor(p);
+		p.getAnnouncements().add(a);
+		announcementRepository.save(a);
+		professorRepository.save(p);
+		return "redirect:/announcement/"+a.getId();
+		
 	}
 	
 }
